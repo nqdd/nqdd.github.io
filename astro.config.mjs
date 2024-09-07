6adb4ae9ee2f2e5da1fe322cb9ce9ec850b6b9f3
Dat Nguyen Quoc <datnq1210@gmail.com>
2024-09-07 12:38:49 +0700

    add deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { remarkReadingTime } from './src/utils/remark-reading-time.mjs';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-theme-cody.netlify.app',
+  site: 'https://nqdd.github.io',
+  base: 'my-blog',
   integrations: [mdx(), sitemap(), tailwind()],
   markdown: {
     remarkPlugins: [remarkReadingTime],
@@ -18,8 +19,6 @@ export default defineConfig({
         light: 'catppuccin-mocha',
         dark: 'catppuccin-latte',
       },
-    }
+    },
   },
 });
-
-
